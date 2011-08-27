1e0a0aff63d2f9395c922bfa0d3c9ed9c5af4e4b
Kevin Gorski <kevin.gorski@gmail.com>
2011-08-27 12:25:41 -0600

    Multiple snakes and refactoring to multiple files

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,10 @@
 window.onload = function () {
-	var tileSize = 16,
-		worldWidth = 25,
-		worldHeight = 21,
-		movers = [];
-	
-    //start crafty
-    Crafty.init(worldWidth * tileSize, worldHeight * tileSize);
-    Crafty.canvas.init();
-
 	Crafty.c('wall', {
 		init: function() {
 			this.addComponent('2D, Canvas, Color, Collision, solid');
 			
-			this.w = tileSize;
-			this.h = tileSize;
+			this.w = Snakes.tileSize;
+			this.h = Snakes.tileSize;
 			
 			this.color('blue')
 				.collision();
@@ -24,40 +15,45 @@ window.onload = function () {
 		init: function() {
 			this.addComponent('2D, Canvas, Color, solid, Collision');
 			
-			this.h = tileSize;
-			this.w = tileSize;
+			this.h = Snakes.tileSize;
+			this.w = Snakes.tileSize;
+		},
+		SnakeSegment: function(color) {
+			this.color(color);
 			
-			this.color('green');
+			return this;
 		}
 	});
 	
 	Crafty.c('Snake', {
-		_segments: [],
-		_direction: 'right',
-		_drawn: false,
-		_keys: {},
-		
 		init: function() {
 			this.addComponent('SnakeSegment');
-			this.h = tileSize;
-			this.w = tileSize;
+			this.h = Snakes.tileSize;
+			this.w = Snakes.tileSize;
 			
-			movers.push(this);
+			this._segments	= [];
+			this._direction	= 'right';
+			this._drawn		= false;
+			this._keys		= {};
+			
+			Snakes.movers.push(this);
 		},
-		_snake: function(length, direction) {
+		Snake: function(length, direction, color) {
 			this._direction = direction;
+
+			this.color(color);
 
 			// The "head" is its own segment
 			for (var i=0; i < length - 1; i++) {
 				this._segments.push(
 					Crafty.e('SnakeSegment')
+						.SnakeSegment(color)
 						.attr({
 							x: this.x,
 							y: this.y,
 							z: this.z
 						}));
 			};
-			
 
 			this.bind("EnterFrame",function() {
 				if(this.disableControls) return;
@@ -66,32 +62,32 @@ window.onload = function () {
 				var prevX = this.x,
 					prevY = this.y,
 					tail = this._segments.pop();
-				
+
 				tail.attr({
 						x: this.x,
 						y: this.y
 					});
-				
+
 				this._segments.unshift(tail);
-				
+
 				switch(this._direction) {
 					case 'up':
-						this.y -= tileSize;
+						this.y -= Snakes.tileSize;
 						break;
-						
+
 					case 'down':
-						this.y += tileSize;
+						this.y += Snakes.tileSize;
 						break;
-						
+
 					case 'left':
-						this.x -= tileSize;
+						this.x -= Snakes.tileSize;
 						break;
-						
+
 					case 'right':
-						this.x += tileSize;
+						this.x += Snakes.tileSize;
 						break;
 				}
-				
+
 				this._drawn = true;
 				this.trigger('Moved', {x: prevX, y: prevY});
 			});
@@ -100,9 +96,6 @@ window.onload = function () {
 					if(this.hit('solid'))
 						this.attr({ x: from.x, y: from.y });
 				});
-		},
-		Snake: function(length, direction) {
-			this._snake(length, direction);
 			
 			return this;
 		}
@@ -111,15 +104,15 @@ window.onload = function () {
 	Crafty.c('Player', {
 		init: function() {
 			this.addComponent('Snake');
-			this.h = tileSize;
-			this.w = tileSize;
+			this.h = Snakes.tileSize;
+			this.w = Snakes.tileSize;
 			
-			movers.push(this);
+			Snakes.movers.push(this);
 		},
-		Player: function(length, direction, keys) {
+		Player: function(length, direction, color, keys) {
 			this._keys = keys;
 			
-			this._snake(length, direction);
+			this.Snake(length, direction, color);
 						
 			this.bind("KeyDown", function(e) {
 				if(this._keys[e.key]) {
@@ -133,58 +126,11 @@ window.onload = function () {
 			});
 		}
 	});
-
-    //method to generate the map
-    function generateWorld() {
-        //loop through all tiles
-        for (var i = 0; i < worldWidth; i++) {
-            for (var j = 0; j < worldHeight; j++) {
-				// Outer walls
-                if(i === 0 || i === 24 || j === 0 || j === 20) {
-                    Crafty.e('wall')
-                    	.attr({ 
-							x: i * tileSize, 
-							y: j * tileSize, 
-							z:2
-						});
-				}
-            }
-        }
-
-		setInterval(function() {
-			for (var i = movers.length - 1; i >= 0; i--){
-				movers[i]._drawn = false;
-			};
-		}, 250);
-    }
-
-    //the loading screen that will display while our assets load
-    Crafty.scene("loading", function () {
 	
-        Crafty.e("2D, DOM, Text").attr({ w: tileSize * worldWidth, h: 20 })
-			.text("Loading")
-			.css({ 
-				"text-align": "center", 
-				"top": "120px",
-				"color": "White" });
+    //start crafty
+    Crafty.init(Snakes.worldWidth * Snakes.tileSize, Snakes.worldHeight * Snakes.tileSize);
+    Crafty.canvas.init();
 
-		Crafty.scene("main"); //when everything is loaded, run the main scene
-    });
-
-    Crafty.scene("main", function () {
-		Crafty.background("black");
-
-		generateWorld();
-
-		Crafty.e('Player')
-			.attr({ 
-				x: tileSize * 3, 
-				y: tileSize, 
-				z: 1
-			}).Player(3, 'right', { 38: 'up', 40: 'down', 39: 'right', 37: 'left'});
-    });
-
-	
-	Crafty.scene("loading");
 	Crafty.background("#000");
+	Crafty.scene("loading");
 };
