66f175f07c52c289c03728c29259445339589420
Eoin Coffey <ecoffey@gmail.com>
2011-08-26 20:11:10 -0600

    server is now saying hello

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-require('nko')('kXMcKF9DfRvjNGzS')
+require('nko')('kXMcKF9DfRvjNGzS');
+
+var express = require('express');
+
+var app = express.createServer();
+
+app.listen(3000);
+
+app.configure(function() {
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname);
+});
+
+app.get('/', function(req, res) {
+  res.end("Oh hai");
+});
+
